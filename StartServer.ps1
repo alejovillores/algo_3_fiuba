951427386238da744f92f15388186bef41b88581
alejovillores <avillores@fi.uba.ar>
2023-11-10 16:08:25 -0300

    Add Powershell script to start server

---
+++ StartServer.ps1
@@ -0,0 +1,36 @@
+# Function to write colored output
+function Write-ColoredText {
+    param (
+        [string]$text,
+        [string]$color
+    )
+    Write-Host $text -ForegroundColor $color
+}
+
+# Set the DEV environment variable
+Write-ColoredText "Setting DEV environment variable" "Magenta"
+$env:DEV = $true
+
+# Run 'pelican content' to create the appropriate output
+Write-ColoredText "Running 'pelican content'..." "Magenta"
+$pelicanContentResult = pelican content
+
+# Check the exit code of 'pelican content'
+if ($LASTEXITCODE -ne 0) {
+    Write-ColoredText "Error: 'pelican content' failed." "Red"
+    Write-Host $pelicanContentResult
+} else {
+    # Get command line arguments
+    $arguments = $args
+
+    # Check if the --reload flag is present
+    if ($arguments -contains "-R" -or $arguments -contains "--reload"  ) {
+        Write-ColoredText "Reload flag detected. Running 'pelican --autoreload --listen'." "Green"
+        pelican --autoreload --listen
+    } else {
+        Write-ColoredText "No reload flag detected. Running 'pelican --listen'." "Green"
+        pelican --listen
+    }
+}
+
+
